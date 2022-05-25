9f08e3dc50de650444451192e62fb2ccc495a87e
Adri <104121249+Mikata95@users.noreply.github.com>
2022-05-25 11:52:28 +0900

    Add files via upload

---
+++ SingleNeuron.java
@@ -0,0 +1,106 @@
+import static java.lang.Math.exp;
+
+public class SingleNeuron {
+
+    private static final int TEACH_NUM = 4;
+    private static final int INP_NUM = 2;
+    private static final double alpha = 0.01;
+
+    static double[] w = new double[INP_NUM];
+    static double[] dw = new double[INP_NUM];
+    static double theta;
+    static double d_theta;
+
+    static double[][] teachX = {
+            { 0, 0 },
+            { 0, 1 },
+            { 1, 0 },
+            { 1, 1 }
+    };
+
+    static double[] teachY = { 0, 1, 0, 1 };
+
+    static double randOne() {
+        return Math.random();
+    }
+
+    static double sigmoid(double x) {
+        return 1.0 / (1.0 + exp(-x));
+    }
+
+    static double forward(double[] x) {
+        double u = 0;
+        for (int i = 0; i < INP_NUM; i++) {
+            u += w[i] * x[i];
+        }
+        u += theta;
+        return sigmoid(u);
+    }
+
+    static double funcError() {
+        double e = 0;
+        for (int t = 0; t < TEACH_NUM; t++) {
+            double y = forward(teachX[t]);
+            e += 0.5 * (y - teachY[t]) * (y - teachY[t]);
+        }
+        return e;
+    }
+
+    static void clearDw() {
+        for (int i = 0; i < INP_NUM; i++) {
+            dw[i] = 0;
+        }
+        d_theta = 0;
+    }
+
+    static void calcDw() {
+        for (int t = 0; t < TEACH_NUM; t++) {
+            double[] xt = teachX[t];
+
+            double y = forward(xt);
+            double y_hat = teachY[t];
+            for (int i = 0; i < INP_NUM; i++) {
+                dw[i] += (y - y_hat) * y * (1 - y) * xt[i];
+            }
+            d_theta += (y - y_hat) * y * (1 - y);
+        }
+    }
+
+    static void initW() {
+        for (int i = 0; i < INP_NUM; i++) {
+            w[i] = randOne() * 2 - 1.0;
+        }
+        theta = randOne() * 2 - 1.0;
+    }
+
+    static void updateW() {
+        for (int i = 0; i < INP_NUM; i++) {
+            w[i] -= alpha * dw[i];
+        }
+        theta -= alpha * d_theta;
+    }
+
+    public static void main(String[] args) {
+        int t = 0, loop = 0;
+
+        initW();
+        for (t = 0; t < TEACH_NUM; t++) {
+            double y = forward(teachX[t]);
+            System.out.format("%d: y = %f <--> y_hat = %f\n", t, y, teachY[t]);
+        }
+        for (loop = 0; loop < 100000; loop++) {
+            if (loop % 1000 == 0) {
+                System.out.format("%d, %f\n", loop, funcError());
+            }
+            clearDw();
+            calcDw();
+            updateW();
+        }
+        System.out.format("%d, %f\n", loop, funcError());
+
+        for (t = 0; t < TEACH_NUM; t++) {
+            double y = forward(teachX[t]);
+            System.out.format("%d: y = %f <--> y_hat = %f\n", t, y, teachY[t]);
+        }
+    }
+}
